e4b632c9368f0d36bcebcd085eaa7923d3cb95d1
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-11-01 12:48:31 +0330

    Add SupportedCurrencies Behaviour

--- _Example/mo-coin/apps/coin/coinBehaviours.ts
+++ _Example/mo-coin/apps/coin/coinBehaviours.ts
@@ -9,20 +9,79 @@ export interface CoinType {
   price_change_percentage_24h: number;
   price_change_percentage_7d_in_currency: number;
   last_updated: Date;
-  circulating_supply: number;  
+  circulating_supply: number;
 }
 
 export interface CoinRequestType {
-  vs_currency: string
-  page: number
-  per_page: number
-  price_change_percentage: string
+  vs_currency: string;
+  page: number;
+  per_page: number;
+  price_change_percentage: string;
 }
 
-export interface CoinSuccessfulResponseType {
+export interface CoinSuccessfulResponseType {}
 
-}
+export interface CoinUnsuccessfulResponseType {}
 
-export interface CoinUnsuccessfulResponseType {
-
-}
+type SupportedCurrencies =
+  | 'btc'
+  | 'eth'
+  | 'ltc'
+  | 'bch'
+  | 'bnb'
+  | 'eos'
+  | 'xrp'
+  | 'xlm'
+  | 'link'
+  | 'dot'
+  | 'yfi'
+  | 'usd'
+  | 'aed'
+  | 'ars'
+  | 'aud'
+  | 'bdt'
+  | 'bhd'
+  | 'bmd'
+  | 'brl'
+  | 'cad'
+  | 'chf'
+  | 'clp'
+  | 'cny'
+  | 'czk'
+  | 'dkk'
+  | 'eur'
+  | 'gbp'
+  | 'hkd'
+  | 'huf'
+  | 'idr'
+  | 'ils'
+  | 'inr'
+  | 'jpy'
+  | 'krw'
+  | 'kwd'
+  | 'lkr'
+  | 'mmk'
+  | 'mxn'
+  | 'myr'
+  | 'ngn'
+  | 'nok'
+  | 'nzd'
+  | 'php'
+  | 'pkr'
+  | 'pln'
+  | 'rub'
+  | 'sar'
+  | 'sek'
+  | 'sgd'
+  | 'thb'
+  | 'try'
+  | 'twd'
+  | 'uah'
+  | 'vef'
+  | 'vnd'
+  | 'zar'
+  | 'xdr'
+  | 'xag'
+  | 'xau'
+  | 'bits'
+  | 'sats';
