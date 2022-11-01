331f4d5b849faad3c6addd526554681789862dec
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-11-01 11:29:52 +0330

    add coin project with next and react-query

---
+++ _Example/mo-coin/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default Home;
