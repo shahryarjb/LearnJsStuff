d35fe1d216c8f6335e4c42b1ec5ed92dc45f81a1
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-11-03 18:38:58 +0330

    delete search coin

--- _Example/mo-coin/template/client/home/components/HeaderPartial.tsx
+++ _Example/mo-coin/template/client/home/components/HeaderPartial.tsx
@@ -28,24 +28,6 @@ const HeaderPartial: NextPage<{supportedCoins: () => void, resetData: () => void
 					callBack={() => resetData()}
 				/>
 			</div>
-
-			<div className="flex justify-center mb-5" id="search">
-				<div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
-					<input
-						type="search"
-						className="form-control relative flex-auto min-w-0 block px-3 py-1.5 text-base font-normal text-black dark:text-gray-300 bg-transparent bg-clip-padding border border-solid border-gray-300 rounded-l transition ease-in-out m-0 focus:text-black focus:bg-transparent dark:focus:text-white dark:focus:border-white focus:outline-none"
-						placeholder="Search a Coin"
-						aria-label="Search"
-						aria-describedby="button-addon3"
-					/>
-					<button
-						className="btn inline-block px-6 py-2 bg-gray-200 rounded-r text-gray-600 font-medium text-xs leading-tight uppercase  hover:bg-gray-300 hover:text-black focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-						type="button"
-						id="button-addon3">
-						Search
-					</button>
-				</div>
-			</div>
 		</>
 	);
 };
