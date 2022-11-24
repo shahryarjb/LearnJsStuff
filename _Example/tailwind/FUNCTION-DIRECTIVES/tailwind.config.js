071daeb73cd8cbcc7fadf4ea47d7c031db2fda56
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-11-24 17:41:39 +0330

    update tailwind

---
+++ _Example/tailwind/FUNCTION-DIRECTIVES/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing: {
+        128: '32rem',
+      },
+    },
+  },
+  plugins: [],
+};
