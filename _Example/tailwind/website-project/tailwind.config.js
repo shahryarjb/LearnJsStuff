0c7fb67e24c28acf6ffc6f2ea2b72706fcb54154
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-11-27 14:35:53 +0330

    add new example for tailwind

---
+++ _Example/tailwind/website-project/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+};
