3957219385c026506d5ec23a33c93937c806c6f6
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-11-30 16:27:39 +0330

    add new project for tailwind

---
+++ _Example/tailwind/testmonial-grid/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+};
