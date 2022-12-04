199f99edd756f2e1ab4fa33fd156b2fc3089ce9f
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-12-04 10:44:19 +0330

    update microfront project

--- _Example/microfronted/ecomm/mfp/packages/container/src/App.js
+++ _Example/microfronted/ecomm/mfp/packages/container/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
